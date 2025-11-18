cec2a8f140b70021956bda7ec8278ed6973dda08
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-11-17 23:48:09 -0300

    adicionando lembrete de que é da clinica tercio resende

--- src/screens/users/index.tsx
+++ src/screens/users/index.tsx
@@ -1,24 +1,37 @@
-import { View, Text, Pressable, Alert, TextInput, Modal,FlatList, ScrollView } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import tw from 'twrnc';
-import { store, USERS_TABLE, initializeStore, clearTable } from "../../config/store";
-import { Input } from '../../components/input/Input';
-
+import {
+  View,
+  Text,
+  Pressable,
+  Alert,
+  TextInput,
+  Modal,
+  FlatList,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import tw from "twrnc";
+import {
+  store,
+  USERS_TABLE,
+  initializeStore,
+  clearTable,
+} from "../../config/store";
+import { Input } from "../../components/input/Input";
 
 interface User {
   id: string;
   name: string;
   email: string;
   phone: string;
-  nota:string
+  nota: string;
 }
 
 export function Users() {
   const [users, setUsers] = useState<User[]>([]);
   const [modalVisible, setModalVisible] = useState<boolean>(false);
   const [uploadModalVisible, setUploadModalVisible] = useState<boolean>(false);
-  const [password, setPassword] = useState<string>('');
-  const [nota, setNota] = useState("")
+  const [password, setPassword] = useState<string>("");
+  const [nota, setNota] = useState("");
 
   const CORRECT_KEY = "Fala1234@";
 
@@ -37,7 +50,7 @@ export function Users() {
     if (password === CORRECT_KEY) {
       handleClear();
       setModalVisible(false);
-      setPassword('');
+      setPassword("");
     } else {
       Alert.alert("Senha incorreta", "A chave digitada está incorreta.");
     }
@@ -45,17 +58,20 @@ export function Users() {
 
   const UpdateItems = async (user: User): Promise<void> => {
     try {
-      const response = await fetch("https://nasago.bubbleapps.io/version-test/api/1.1/wf/form_totem", {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(user)
-      });
+      const response = await fetch(
+        "https://nasago.bubbleapps.io/version-test/api/1.1/wf/form_totem",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(user),
+        }
+      );
 
       const json = await response.json();
       console.log(json);
     } catch (error) {
       Alert.alert("Erro de conexão ou inesperado.");
-      console.error('Erro:', error);
+      console.error("Erro:", error);
     }
   };
 
@@ -64,7 +80,7 @@ export function Users() {
       await UpdateItems(item);
     }
     Alert.alert("Dados enviados com sucesso!");
-    setNota("")
+    setNota("");
   };
 
   const get = (): void => {
@@ -75,7 +91,7 @@ export function Users() {
       email: String(user.email),
       phone: String(user.phone),
       game: String("Roleta"),
-      nota: String(nota)
+      nota: String("Leads da clinica tercio resende"),
     }));
     setUsers(response);
     // console.log('📦 Dados atuais:', data); //Mostrar dados no console
@@ -95,137 +111,179 @@ export function Users() {
   }, [nota]);
 
   return (
-    <ScrollView scrollEnabled={true} >
-    <View style={tw`flex-1 items-center mt-8 px-4`}>
-      <Text style={tw`text-xl font-medium`}>Inscritos</Text>
-      <View style={tw`mt-6 w-full`}>
-        {users.length > 0 && (
-          <View style={tw`flex-row justify-between items-center`}>
-            <Text style={tw`text-base text-center min-w-30`}>Nome</Text>
-            <Text style={tw`text-base text-center min-w-30`}>Email</Text>
-            <Text style={tw`text-base text-center min-w-30`}>Telefone</Text>
-          </View>
-        )}
-        
-        <FlatList
-        data={users}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => {
-          const id = item;
-          return (
-            <View >
-              <View style={{flexDirection:"row", justifyContent:"space-evenly"}}>
-                <Text style={{fontSize:12, fontWeight:500, color:"#333333"}}> {item.name}</Text>
-                <Text style={{fontSize:12, fontWeight:500, color:"#333333"}}>{item.email}</Text>
-                <Text style={{fontSize:12, fontWeight:500, color:"#333333"}}> {item.phone}</Text>
-                
+    <ScrollView scrollEnabled={true}>
+      <View style={tw`flex-1 items-center mt-8 px-4`}>
+        <Text style={tw`text-xl font-medium`}>Inscritos</Text>
+        <View style={tw`mt-6 w-full`}>
+          {users.length > 0 && (
+            <View style={tw`flex-row justify-between items-center`}>
+              <Text style={tw`text-base text-center min-w-30`}>Nome</Text>
+              <Text style={tw`text-base text-center min-w-30`}>Email</Text>
+              <Text style={tw`text-base text-center min-w-30`}>Telefone</Text>
+            </View>
+          )}
+
+          <FlatList
+            data={users}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => {
+              const id = item;
+              return (
+                <View>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-evenly",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        fontWeight: 500,
+                        color: "#333333",
+                      }}
+                    >
+                      {" "}
+                      {item.name}
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        fontWeight: 500,
+                        color: "#333333",
+                      }}
+                    >
+                      {item.email}
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        fontWeight: 500,
+                        color: "#333333",
+                      }}
+                    >
+                      {" "}
+                      {item.phone}
+                    </Text>
+                  </View>
+                </View>
+              );
+            }}
+          />
+          {users.length > 0 && (
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginBottom: 20,
+              }}
+            >
+              <Pressable
+                style={tw`bg-purple-500 p-4 rounded-md`}
+                onPress={() => setUploadModalVisible(true)}
+              >
+                <Text style={tw`text-white font-bold`}>Subir dados</Text>
+              </Pressable>
+
+              <Pressable
+                style={tw`bg-red-500 p-4 rounded-md`}
+                onPress={() => setModalVisible(true)}
+              >
+                <Text style={tw`text-white font-bold`}>Limpar dados</Text>
+              </Pressable>
+            </View>
+          )}
+        </View>
+
+        {/* 🔐 Modal limpar */}
+        <Modal
+          transparent
+          visible={modalVisible}
+          animationType="slide"
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={tw`flex-1 justify-center items-center bg-black/50`}>
+            <View style={tw`bg-white p-6 rounded-lg w-80`}>
+              <Text style={tw`text-lg font-bold mb-4`}>
+                Digite a chave para limpar
+              </Text>
+              <TextInput
+                placeholder="Digite aqui..."
+                style={tw`border border-gray-400 rounded-md p-2 mb-4`}
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+              />
+              <View style={tw`flex-row justify-between`}>
+                <Pressable
+                  style={tw`bg-gray-400 px-4 py-2 rounded-md`}
+                  onPress={() => {
+                    setModalVisible(false);
+                    setPassword("");
+                  }}
+                >
+                  <Text style={tw`text-white font-bold`}>Cancelar</Text>
+                </Pressable>
+                <Pressable
+                  style={tw`bg-red-500 px-4 py-2 rounded-md`}
+                  onPress={handleClearConfirmation}
+                >
+                  <Text style={tw`text-white font-bold`}>Confirmar</Text>
+                </Pressable>
               </View>
             </View>
-          );
-        }}
-      />
-        {users.length > 0 && (
-          <View style={{flexDirection:"row", justifyContent:"space-between", marginBottom:20}}>
-            <Pressable
-              style={tw`bg-purple-500 p-4 rounded-md`}
-              onPress={() => setUploadModalVisible(true)}
-            >
-              <Text style={tw`text-white font-bold`}>Subir dados</Text>
-            </Pressable>
-
-            <Pressable
-              style={tw`bg-red-500 p-4 rounded-md`}
-              onPress={() => setModalVisible(true)}
-            >
-              <Text style={tw`text-white font-bold`}>Limpar dados</Text>
-            </Pressable>
           </View>
-        )}
-      </View>
+        </Modal>
 
-      {/* 🔐 Modal limpar */}
-      <Modal
-        transparent
-        visible={modalVisible}
-        animationType="slide"
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View style={tw`flex-1 justify-center items-center bg-black/50`}>
-          <View style={tw`bg-white p-6 rounded-lg w-80`}>
-            <Text style={tw`text-lg font-bold mb-4`}>
-              Digite a chave para limpar
-            </Text>
-            <TextInput
-              placeholder="Digite aqui..."
-              style={tw`border border-gray-400 rounded-md p-2 mb-4`}
-              secureTextEntry
-              value={password}
-              onChangeText={setPassword}
-            />
-            <View style={tw`flex-row justify-between`}>
-              <Pressable
-                style={tw`bg-gray-400 px-4 py-2 rounded-md`}
-                onPress={() => {
-                  setModalVisible(false);
-                  setPassword('');
-                }}
-              >
-                <Text style={tw`text-white font-bold`}>Cancelar</Text>
-              </Pressable>
-              <Pressable
-                style={tw`bg-red-500 px-4 py-2 rounded-md`}
-                onPress={handleClearConfirmation}
-              >
-                <Text style={tw`text-white font-bold`}>Confirmar</Text>
-              </Pressable>
-            </View>
-          </View>
-        </View>
-      </Modal>
-
-      {/* 🚀 Modal Upload */}
-      <Modal
-        transparent
-        visible={uploadModalVisible}
-        animationType="slide"
-        onRequestClose={() => setUploadModalVisible(false)}
-      >
-        <View style={tw`flex-1 justify-center items-center bg-black/50`}>
-          <View style={{backgroundColor:"#FFFFFF", borderRadius:16, padding:24, gap:8, width:350}}>
-            <Text style={tw`text-lg font-bold mb-4`}>
-              
-              Deseja realmente enviar os dados?
-            </Text>
-            <Text style={tw`mb-4`}>
-              Isso irá enviar todos os inscritos para o servidor.
-            </Text>
-            <Input place='Digite uma nota'
-              value={nota}
-              onChangeText={(value)=> setNota(value)
-              }
+        {/* 🚀 Modal Upload */}
+        <Modal
+          transparent
+          visible={uploadModalVisible}
+          animationType="slide"
+          onRequestClose={() => setUploadModalVisible(false)}
+        >
+          <View style={tw`flex-1 justify-center items-center bg-black/50`}>
+            <View
+              style={{
+                backgroundColor: "#FFFFFF",
+                borderRadius: 16,
+                padding: 24,
+                gap: 8,
+                width: 350,
+              }}
+            >
+              <Text style={tw`text-lg font-bold mb-4`}>
+                Deseja realmente enviar os dados?
+              </Text>
+              <Text style={tw`mb-4`}>
+                Isso irá enviar todos os inscritos para o servidor.
+              </Text>
+              <Input
+                place="Digite uma nota"
+                value={nota}
+                onChangeText={(value) => setNota(value)}
               />
-            <View style={tw`flex-row justify-between`}>
-              <Pressable
-                style={tw`bg-gray-400 px-4 py-2 rounded-md`}
-                onPress={() => setUploadModalVisible(false)}
-              >
-                <Text style={tw`text-white font-bold`}>Cancelar</Text>
-              </Pressable>
-              <Pressable
-                style={tw`bg-purple-500 px-4 py-2 rounded-md`}
-                onPress={() => {
-                  setUploadModalVisible(false);
-                  loopUpdateItems();
-                }}
-              >
-                <Text style={tw`text-white font-bold`}>Confirmar</Text>
-              </Pressable>
+              <View style={tw`flex-row justify-between`}>
+                <Pressable
+                  style={tw`bg-gray-400 px-4 py-2 rounded-md`}
+                  onPress={() => setUploadModalVisible(false)}
+                >
+                  <Text style={tw`text-white font-bold`}>Cancelar</Text>
+                </Pressable>
+                <Pressable
+                  style={tw`bg-purple-500 px-4 py-2 rounded-md`}
+                  onPress={() => {
+                    setUploadModalVisible(false);
+                    loopUpdateItems();
+                  }}
+                >
+                  <Text style={tw`text-white font-bold`}>Confirmar</Text>
+                </Pressable>
+              </View>
             </View>
           </View>
-        </View>
-      </Modal>
-    </View>
+        </Modal>
+      </View>
     </ScrollView>
   );
 }
-
